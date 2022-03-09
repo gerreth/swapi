a42bc4a3a6257d40fbc66426a36f19a30dcaa6a1
Gerret Halberstadt <gerreth@gmx.net>
2022-03-09 14:52:04 +0100

    Add material ui

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,15 +1,39 @@
 import React from "react";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 
 import client from "../utils/client";
 
 import "../styles/globals.css";
 
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#000000",
+    },
+  },
+  typography: {
+    body1: {
+      fontWeight: "400",
+      fontSize: "0.875rem",
+    },
+    body2: {
+      fontWeight: "700",
+      fontSize: "0.875rem",
+    },
+  },
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </ApolloProvider>
   );
 }
